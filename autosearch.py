f052c70416b608bec2f95116c5f66e01d8dabe23
caozhzh@office <caozhzh@gmail.com>
2013-04-16 15:53:26 +0800

    write to file and chart basiclly ok

--- autosearch.py
+++ autosearch.py
@@ -10,13 +10,14 @@ INTERNAL=0
 NUM=10
 PREFERENCE="autosearch.conf"
 FORMAT=""
-WRITE=""
+WRITE="autosearch.output"
 FILTER=""
 CHART=""
 SORT=""
 VERBOSE=False
 
 ENGINES=['google', 'baidu', 'qihoo', 'maopu', 'tianya', 'weibo', 'tq']
+DATA=[]
 
 def opt():
     global KEYWORD, ENGINE, INTERNAL, NUM, PREFERENCE, FORMAT, WRITE, FILTER, CHART, SORT, VERBOSE
@@ -34,6 +35,8 @@ def opt():
             	if not e in ENGINES:
             		print "warning: %s is not supported, ignored" % e
             		ENGINE.remove(e)
+            	else:
+            		DATA.append([e, []])
         elif op == "-i" or op == "--internal":
             INTERNAL = float(value)
         elif op == "-n" or op == "--num":
@@ -64,14 +67,41 @@ def usage():
     -e,--engine=google,baidu,qihoo,maopu,tianya,weibo,tq ---at least one of these search engines
     -i,--internal=NUMBER ---seconds, default is 0
     -n,--num=NUMBER ---topmost search results, default is 10
-    -p,--preference=FILE ---set preference file name, like autosearch.conf
+    -p,--preference=FILE ---set preference file name, default is autosearch.conf
     -o,--format=title,url,desc ---set output formate
-    -w,--write=FILE ---set output file, like autosearch.output
+    -w,--write=FILE ---set output file, default is autosearch.output
     -f,--filter=STRING ---set filter string
     -c,--chart ---got output chart
     -s,--sort ---set sort type
     -v,--verbose ---if set, print the output to screen also
     -h,--help ---this help information""" 
+
+def try_output(content):
+	#s = str.format( content )
+	if VERBOSE:
+		print content
+	if WRITE:
+		with open(WRITE, 'a+') as f:
+			f.write(content.encode('utf-8'))
+			f.write("\n")
+
+
+import cairo
+import pycha
+import pycha.bar
+def try_chart():
+	global DATA
+	#print DATA
+
+	if CHART=='':
+		return
+
+	width, height = (500,400)
+	surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+	chart = pycha.bar.VerticalBarChart(surface)
+	chart.addDataset( DATA )
+	chart.render()
+	surface.write_to_png('output.png')
 
 try:
 	opt()
@@ -94,20 +124,29 @@ if len(ENGINE)==0:
 	usage()
 	sys.exit()
 
-def try_output(content):
-	if VERBOSE:
-		print content
+if WRITE:
+	with open(WRITE, 'w') as f:
+		pass
 
 ### main logic
 from xgoogle.GeneralSearch import GeneralSearch
 import time
 import string
+import time
 
+start_time = 0
 while True:
 	for e in ENGINE:
 		gs=GeneralSearch(KEYWORD, e)
 		results = gs.get_results()
 		print gs.num_results
+		try_output( "%s: %d results of \"%s\" --- %s" % ( e.upper(), gs.num_results, KEYWORD, time.strftime("%Y-%m-%d %X", time.localtime())) )
+		try_output( 80*'-' )
+
+		index = ENGINE.index(e)
+		if DATA[index][0]==e:
+			#DATA[index][1].append([time.strftime("%Y-%m-%d %X", time.localtime()), gs.num_results])
+			DATA[index][1].append([start_time, gs.num_results])
 
 		count=0
 		over=False
@@ -117,23 +156,22 @@ while True:
 				if count>NUM:
 					over=True
 					break
-				if VERBOSE:
-					print 30*'+'
-					print "results[%d]: " % count
+				try_output( "results[%d]: " % count )
 				for k in ['title', 'url', 'desc']:
-					if VERBOSE:
-						s = "r.%s" % k
-						print "%s: %s" % ( k, eval(s) )
-				if VERBOSE:
-					print 30*'+'
+					s = "r.%s" % k
+					try_output( "%s: %s" % ( k, eval(s) ) )
+				try_output( 80*'+' )
 			if over:
 				break
 			results = gs.get_results()
 
 		del gs
 
+	try_chart()
+
 	if INTERNAL:
 		print "info: loop search every %d seconds, press CTRL+C to exit." % INTERNAL
 		time.sleep(string.atof(INTERNAL))
+		start_time += INTERNAL
 	else:
 		sys.exit()
